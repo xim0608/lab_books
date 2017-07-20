24a99ff455fe4ee1ae76e99aac3ea8484b2fec70
RyukiWada <xim.ryuki@gmail.com>
2017-07-20 12:01:14 +0900

    revert

--- db/migrate/20170719235855_add_and_delete_column_to_rentals.rb
+++ db/migrate/20170719235855_add_and_delete_column_to_rentals.rb
@@ -2,12 +2,12 @@ class AddAndDeleteColumnToRentals < ActiveRecord::Migration[5.1]
   def up
     remove_column :rentals, :soft_destroyed_at
     add_column :rental_histories, :unread, :boolean, null: false, default: true
-    add_column :rentals, :unread, :boolean, null: false, default: true
+    # add_column :rentals, :unread, :boolean, null: false, default: true
   end
 
   def down
     add_column :rentals, :soft_destroyed_at, :datetime
     remove_column :rental_histories, :unread
-    remove_column :rentals, :unread
+    # remove_column :rentals, :unread
   end
 end
