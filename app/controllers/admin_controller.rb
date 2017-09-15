b56da9472c47828f07e4c972a8a221d4287e650b
RyukiWada <xim.ryuki@gmail.com>
2017-09-15 19:24:55 +0900

    adminユーザー一覧表示

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,11 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all
+    @b3s = User.where(year: 'B3')
+    @b4s = User.where(year: 'B4')
+    @m1s = User.where(year: 'M1')
+    @m2s = User.where(year: 'M2')
+    @books_sum = Rental.group(:user_id).sum(:user_id)
   end
+
 end
