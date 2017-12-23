cccb966dce75776f2be4c7531c0f05f6b9f2c8f0
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-23 14:33:27 +0900

    modify devise lang

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Labooks
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
   end
 end
