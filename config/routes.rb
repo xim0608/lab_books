1429d3c8c55f75dfe6f0b13cb44bc308c9c6a9de
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-23 19:12:25 +0900

    disable to login from home

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 
-  devise_for :users,
-             skip: [:session, :registration] # ,
-             # controllers: {omniauth_callbacks: "users/omniauth_callbacks",
-             #               registrations: "users/registrations",
-             #               sessions: "users/sessions"  }
-
   devise_scope :user do
-    get '/users/sign_in' => 'devise/sessions#new', as: :new_user_session
-    post '/users/sign_in' => 'devise/sessions#create', as: :user_session
-    delete '/users/sign_out' => 'devise/sessions#destroy', as: :destroy_user_session
-    post '/users' => 'users/registrations#create', as: :user_registration
-    get '/users/edit' => 'users/registrations#edit', as: :edit_user_registration
-    get '/users/edit' => 'users/registrations#new', as: :new_user_registration
-    patch '/users' => 'users/registrations#update', as: nil
-    put '/users' => 'users/registrations#update', as: :update_user_registration
-    delete '/users' => 'users/registrations#destroy', as: :destroy_user_registration
+    get '/sign_in' => 'devise/sessions#new'
   end
 
+  # disable sign_up from top_page
+  devise_for :users, skip: [:registrations]
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    end
 
   resources :books, except: [:edit, :update, :delete] do
     collection do
@@ -41,7 +33,6 @@ Rails.application.routes.draw do
     end
   end
 
-  # resource :admins, except: [:show, :destroy, :create, :edit, :update, :delete, :new] do
   resources :admins, only: [:index] do
     collection do
       get :users
@@ -49,18 +40,8 @@ Rails.application.routes.draw do
       get 'users/:user_id/:book_id', to: 'admins#change_flag', as: 'admin_change_rental_flag'
     end
   end
-  # get 'admins/users'
-
-
-  # resources :api, except: [:all] do
-  #   collection do
-  #     post :search
-  #   end
-  # end
 
   get 'api/inc_search', to: 'api#inc_search'
-  # post 'api/rent', to: 'api#rent'
-  # post 'api/return', to: 'api#return'
   post 'api/process', to: 'api#data_catcher'
   get 'api/rentals/:student_id', to: 'rentals#show_by_student_id'
   get 'api/rentals/change_flag/:student_id', to: 'rentals#change_unread_flag', as: 'rental_change_unread_flag'
