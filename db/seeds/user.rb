219efc4aab87095ed7e1a948e3b4b5d96fc4c784
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-24 17:08:11 +0900

    implement inserting initial user data by seeds

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,5 +1,8 @@
-print("started to set users")
+p 'started to set users'
 admin_users = User.where(is_admin: true)
 if admin_users.blank?
-  User.create(email: ENV['INITIAL_USER_EMAIL'], password: ENV['INITIAL_USER_PASS'], is_admin: true)
+  user = User.create!(email: ENV['INITIAL_USER_EMAIL'], password: ENV['INITIAL_USER_PASS'], is_admin: true)
+  p "admin_user #{user.email} were created"
+else
+  p 'do nothing'
 end
