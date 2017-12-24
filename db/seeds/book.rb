fe47bc87736159823d35f06e315dcd54211676c0
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-24 17:07:41 +0900

    implement book import from csv by seeds

--- db/seeds/book.rb
+++ db/seeds/book.rb
@@ -1,1 +1,11 @@
-print("started to set books")
+require 'csv'
+
+p "started to set books"
+csv_dir = Dir.glob(File.join(Rails.root, 'db', 'seeds', 'booklog*')).last
+
+counter = 0
+File.open(csv_dir, 'r') do |file|
+  p file.path
+  counter = Book.import(file)
+end
+p "#{counter} books were inserted"
