880072fe60d8255770630b3512306dc2665de8af
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-24 12:55:36 +0900

    remove unneeded column (books:user_id)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171223131735) do
+ActiveRecord::Schema.define(version: 20171224035219) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "isbn_13"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20171223131735) do
     t.text "image_url"
     t.integer "publish_year"
     t.integer "pages"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
