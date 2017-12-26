2b6b6927051a97734ca1f22898108be79422d4c3
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-27 02:26:51 +0900

    fix setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,7 @@ set :pty, true
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
 set :default_env, {path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"}
 set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+set :bundle_flags, "--deployment --without development test"
 set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
