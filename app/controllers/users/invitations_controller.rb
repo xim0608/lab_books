e5da4ed5af858e58a104d8ecce2790d18ae8080c
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-29 22:14:04 +0900

    fix error handling

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -4,7 +4,42 @@ class Users::InvitationsController < Devise::InvitationsController
   end
 
   def create
-    super
+    self.resource = invite_resource
+    resource_invited = resource.errors.empty?
+
+    yield resource if block_given?
+
+    @token = resource.invitation_token
+
+    # temporary_mail: 'myjlab@username'
+    temporary_mail = invite_resource.email.split('@')
+    if temporary_mail.size > 1
+      slack_user = temporary_mail[1]
+      @resource = invite_resource
+      text = ERB.new(File.read("#{Rails.root}/app/views/devise/mailer/invitation_instructions.text.erb")).result(binding)
+      res = Slack.chat_postMessage(text: text, username: 'labooks', channel: "@#{slack_user}")
+      Rails.logger.info(res)
+    else
+      res = {'ok': false}
+    end
+
+    if res['ok'] == false
+      redirect_to new_user_invitation_path, alert: "#{slack_user}というユーザはgroupに存在しません"
+    else
+      if resource_invited
+        if is_flashing_format? && self.resource.invitation_sent_at
+          set_flash_message :notice, :send_instructions, :email => self.resource.email
+        end
+        if self.method(:after_invite_path_for).arity == 1
+          respond_with resource, :location => after_invite_path_for(current_inviter)
+        else
+          respond_with resource, :location => after_invite_path_for(current_inviter, resource)
+        end
+      else
+        respond_with_navigational(resource) {render :new}
+      end
+    end
+
   end
 
   private
--- config/initializers/devise_invitable/mailer/mailer.rb
+++ config/initializers/devise_invitable/mailer/mailer.rb
@@ -7,12 +7,6 @@ module DeviseInvitable
 
     # Deliver an invitation email
     def invitation_instructions(record, token, opts={})
-      @token = token
-      slack_user = record.email.split('@')[1]
-      @resource = record
-      text = ERB.new(File.read("#{Rails.root}/app/views/devise/mailer/invitation_instructions.text.erb")).result(binding)
-      res = Slack.chat_postMessage(text: text, username: 'labooks', channel: "@#{slack_user}")
-      Rails.logger.info(res)
       # devise_mail(record, :invitation_instructions, opts)
     end
   end
