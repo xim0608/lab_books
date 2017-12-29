9bee0b2b69fb048b50e22413b7f40515cbd66989
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-29 13:00:20 +0900

    cp to appreciate path

---
+++ config/unicorn/production.rb
@@ -0,0 +1,49 @@
+
+Ω# -*- coding: utf-8 -*-
+worker_processes 1
+
+app_path = '/var/www/labooks'
+app_shared_path = "#{app_path}/shared"
+working_directory = '/var/www/labooks/current'
+
+listen "#{app_shared_path}/tmp/sockets/unicorn.sock"
+
+stdout_path "#{app_shared_path}/log/unicorn.stdout.log"
+stderr_path "#{app_shared_path}/log/unicorn.stderr.log"
+
+pid "#{app_shared_path}/tmp/pids/unicorn.pid"
+
+stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
+timeout 30
+preload_app true
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{working_directory}/Gemfile"
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+# after_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+#   end
+#
+#   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+# end
