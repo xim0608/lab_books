20086a7e27ec19340b7efec615514cd992d8e396
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-29 11:48:51 +0900

    delete some settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -35,10 +35,10 @@ before_fork do |server, worker|
   end
 end
 
-after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
-end
+# after_fork do |server, worker|
+#   Signal.trap 'TERM' do
+#     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+#   end
+#
+#   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+# end
