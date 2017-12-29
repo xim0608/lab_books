111b3ac7995675bbc5802a158b2e96c1a22bec8e
Ryuki Wada <xim.ryuki@gmail.com>
2017-12-30 00:59:39 +0900

    add slack user column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171224035219) do
+ActiveRecord::Schema.define(version: 20171229144703) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "isbn_13"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20171224035219) do
     t.text "tokens"
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
+    t.string "slack_name", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
