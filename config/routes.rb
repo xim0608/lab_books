3f3091c84b043af83a10b1a69fcfa06b2ef0a609
Ryuki Wada <xim.ryuki@gmail.com>
2018-02-02 21:08:32 +0900

    sidekiq controller panel in admin user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
 
+  require 'sidekiq/web'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   devise_scope :user do
     get '/sign_in' => 'devise/sessions#new'
   end
